92da26e9677e26e08bd7f207822f3742e0aafad5
User1 <user1@localhost.localdomain>
2016-08-16 11:15:24 -0600

    I created the sessions controller with actions: login, home, profile, and setting.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/login'
+
+  get 'sessions/home'
+
+  get 'sessions/profile'
+
+  get 'sessions/setting'
+
   get 'users/new'
 
   resources :articles
