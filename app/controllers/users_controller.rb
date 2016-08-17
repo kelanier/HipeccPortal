d3fdd57bc6ef8ec4e62aa72f6e33c99d5dca3e58
User1 <user1@localhost.localdomain>
2016-08-16 18:49:07 -0600

    I added a 'Sign In' link to the 'new user' page, and I also uploaded this app to heroku.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
 
     render "new"
   end
-  
+
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :username, :password, :password_confirmation, :organization, :address_street, :address_city, :address_state, :address_country, :email, :commit)
